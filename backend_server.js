f92b4f0cdbe27555bef47f866f6523a1a013d259
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-06-04 20:48:44 +0200

    TODO app: backendserver coded but not tested

--- backend_server.js
+++ backend_server.js
@@ -16,11 +16,11 @@ var BackendServer = (function() {
             host: "localhost",
             user: "'Fekapapa'",
             password: "1q2w3ezv8ta4",
-            database: "musicplayer"
+            database: "todo"
         });
 
         app.get('/', function(req, res) {
-            res.sendFile(__dirname + '/musicplayer_2.html');
+            res.sendFile(__dirname + '/TODO.html');
         });
 
         app.listen(3000, function () {
@@ -34,15 +34,16 @@ var BackendServer = (function() {
             console.log("Connection established");
         });
 
-        getPlaylists(app, conn);
-        getCurrentTracks(app, conn);
-        putFavoritePlus(app, conn);
-        putFavoriteMinus(app, conn);
+        getTodoData(app, conn);
+        postTodoData(app, conn);
+        postTodoDataDelete(app, conn);
+        putTodoReady(app, conn);
+        putTodoReadyUncheck(app, conn);
     }
 
-    function getPlaylists(app, conn) {
-        app.get('/playlists', function(req, res) {
-            conn.query('SELECT * FROM playlists', function(err,rows){
+    function getTodoData(app, conn) {
+        app.get('/todo_data_get', function(req, res) {
+            conn.query('SELECT * FROM todo_data', function(err,rows){
                 if(err){
                     console.log("Error connecting to Db");
                     console.log(err);
@@ -55,42 +56,36 @@ var BackendServer = (function() {
         });
     }
 
-    // function postPlaylist () {
-    //     app.post ('/playlists', function(req, res) {
-    //         var query = 'INSERT INTO playlists (title, system) VALUES ("'
-    //          + req.body.title + '", "' + req.body.system + '")';
-    //         conn.query(query), function(err,rows){
-    //         };
-    //     });
-    // }
-
-    function getCurrentTracks(app, conn) {
-        app.get('/tracks', function(req, res) {
-            conn.query('SELECT * FROM tracks', function(err,rows){
-                if(err){
-                    console.log("Error connecting to Db");
-                    console.log(err);
-                }else {
-                    console.log("Data received from Db:\n");
-                    console.log(rows);
-                };
-            res.send(rows);
-            });
+    function postTodoData(app, conn) {
+        app.post ('/todo_data_post', function(req, res) {
+            var query = 'INSERT INTO todo_data (title, status) VALUES ("'
+             + req.body.title + '", "' + 0 + '")';
+            conn.query(query), function(err,rows){
+            };
         });
     }
 
-    function putFavoritePlus(app, conn) {
-        app.put ('/tracksfavplus', function(req, res) {
-            var query = 'UPDATE tracks SET favorite=1 WHERE title = ' + '"' + req.body.title + '"';
+    function postTodoDataDelete(app, conn) {
+        app.post ('/todo_data_delete', function(req, res) {
+            var query = 'DELETE FROM todo_data WHERE title = "'
+             + req.body.title + '"';
+            conn.query(query), function(err,rows){
+            };
+        });
+    }
+
+    function putTodoReady(app, conn) {
+        app.put ('/todo_ready_check', function(req, res) {
+            var query = 'UPDATE todo_data SET status=1 WHERE title = ' + '"' + req.body.title + '"';
             console.log(query);
             conn.query(query), function(err,rows){
             };
         });
     }
 
-    function putFavoriteMinus(app, conn) {
-        app.put ('/tracksfavminus', function(req, res) {
-            var query = 'UPDATE tracks SET favorite=0 WHERE title = ' + '"' + req.body.title + '"';
+    function putTodoReadyUncheck(app, conn) {
+        app.put ('/todo_ready_uncheck', function(req, res) {
+            var query = 'UPDATE todo_data SET favorite=0 WHERE title = ' + '"' + req.body.title + '"';
             console.log(query);
             conn.query(query), function(err,rows){
             };
